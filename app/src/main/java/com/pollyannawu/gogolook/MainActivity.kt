ae86a08f7140447dedbe16452afcf7159374cdcd
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-23 23:18:09 +0800

    remove default setting which is unnecessary

--- app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
+++ app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
@@ -15,17 +15,7 @@ import com.pollyannawu.gogolook.ui.theme.GogoLookTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GogoLookTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
     }
 }
 
