6463737a50bffcca532cb63ee053fa070140108d
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-24 14:40:35 +0800

    create Response data class to receive API Response

--- app/src/main/java/com/pollyannawu/gogolook/data/dataclass/Response.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/dataclass/Response.kt
@@ -1,3 +1,33 @@
-package com.pollyannawu.gogolook.data.dataclass
+import com.squareup.moshi.Json
 
-data class Response()
+data class Response(
+    val total: Int,
+    val totalHits: Int,
+    val hits: List<Hit>
+)
+
+data class Hit(
+    val id: Int,
+    val pageURL: String,
+    val type: String,
+    val tags: String,
+    val previewURL: String,
+    val previewWidth: Int,
+    val previewHeight: Int,
+    val webformatURL: String,
+    val webformatWidth: Int,
+    val webformatHeight: Int,
+    val largeImageURL: String,
+    val imageWidth: Int,
+    val imageHeight: Int,
+    val imageSize: Int,
+    val views: Int,
+    val downloads: Int,
+    val collections: Int,
+    val likes: Int,
+    val comments: Int,
+    @Json(name = "user_id")
+    val userId: Int,
+    val user: String,
+    val userImageURL: String
+)
