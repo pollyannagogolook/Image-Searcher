82b283ecf96cdbcd9f66517c826cebc414fb9616
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-24 20:13:59 +0800

    create getImagesFromPixabayAPI function in repository to provide method to fetch data from pixabay

--- app/src/main/java/com/pollyannawu/gogolook/data/model/Repository.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/model/Repository.kt
@@ -1,6 +1,10 @@
 package com.pollyannawu.gogolook.data.model
 
+import com.pollyannawu.gogolook.data.dataclass.Hit
+
 interface Repository {
 
     fun getDefaultLayoutByRemoteConfig(defaultLayoutCallback: (String) -> Unit)
+
+    fun getImagesFromPixabayAPI(hitsCallback: (List<Hit>) -> Unit)
 }
--- app/src/main/java/com/pollyannawu/gogolook/data/model/RepositoryImpl.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/model/RepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.pollyannawu.gogolook.data.model
 
+import com.pollyannawu.gogolook.data.dataclass.Hit
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -7,5 +8,9 @@ import javax.inject.Singleton
 class RepositoryImpl @Inject constructor(private val remoteDataSource: RemoteDataSource): Repository{
     override fun getDefaultLayoutByRemoteConfig(defaultLayoutCallback: (String) -> Unit) {
         remoteDataSource.getDefaultLayoutByRemoteConfig(defaultLayoutCallback)
+    }
+
+    override fun getImagesFromPixabayAPI(hitCallbacks: (List<Hit>) -> Unit) {
+        remoteDataSource.getImagesFromPixabayAPI(hitCallbacks)
     }
 }
