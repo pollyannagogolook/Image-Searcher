1d8af075a85ee487e00893fa98cb6aa7b95faf3c
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-24 20:39:08 +0800

    set base url of pixabay api as local constant variable in ApiService

--- app/src/main/java/com/pollyannawu/gogolook/network/ApiService.kt
+++ app/src/main/java/com/pollyannawu/gogolook/network/ApiService.kt
@@ -1,4 +1,11 @@
 package com.pollyannawu.gogolook.network
 
-class ApiService {
-}
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+
+
+private const val HOST_NAME = "pixabay.com"
+private const val BASE_URL = "https://$HOST_NAME/api"
+
+
