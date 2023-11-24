42ab30b6e324eea25e63dc1b4e7cc3dcb14d48a5
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-24 21:09:43 +0800

    create ApiService interface for provide http method to get images

--- app/src/main/java/com/pollyannawu/gogolook/network/ApiService.kt
+++ app/src/main/java/com/pollyannawu/gogolook/network/ApiService.kt
@@ -4,9 +4,12 @@ import com.pollyannawu.gogolook.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
+import com.pollyannawu.gogolook.data.dataclass.ApiResponse
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 private const val HOST_NAME = "pixabay.com"
@@ -35,3 +38,11 @@ private val retrofit = Retrofit.Builder()
     .client(client)
     .build()
 
+
+interface ApiService {
+    @GET("/")
+    suspend fun searchImages (
+        @Query("q")input: String,
+        @Query("image_type")imageType: String = "photo"
+    ): Response<ApiResponse>
+}
