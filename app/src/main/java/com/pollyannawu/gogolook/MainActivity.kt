07fa14cdb7d7796b293ffb9b91fbd78706017f88
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-24 14:32:20 +0800

    add image recyclerview in activity main layout

--- app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
+++ app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
@@ -2,15 +2,8 @@ package com.pollyannawu.gogolook
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.pollyannawu.gogolook.ui.theme.GogoLookTheme
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,18 +12,5 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GogoLookTheme {
-        Greeting("Android")
-    }
-}
+
