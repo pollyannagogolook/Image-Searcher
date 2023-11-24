7a32d8ccaceb8620497ebb2bc9898456f08d1916
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-25 00:53:17 +0800

    watch user input change by set doAfterTextChanged function in main activity

--- app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
+++ app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.pollyannawu.gogolook
 
+import android.graphics.ColorSpace.Adaptation
 import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.inputmethod.EditorInfo
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
@@ -26,21 +31,41 @@ class MainActivity : ComponentActivity() {
 
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val imageAdapter = ImageAdapter()
+
+        // record user latest input
+        var userInput = ""
 
 
-        // set layout manager after remote setting
+        // set layout manager after getting remote setting value
         lifecycleScope.launch {
             viewModel.defaultLayout.collect{ defaultLayout ->
                 if (defaultLayout == DEFAULT_LAYOUT){
                     binding.imageRecyclerview.layoutManager = LinearLayoutManager(this@MainActivity)
                 }
-                else{
+                else {
                     binding.imageRecyclerview.layoutManager = GridLayoutManager(this@MainActivity, GRID_COUNT_SPAN)
                 }
+                binding.imageRecyclerview.adapter = imageAdapter
             }
+
+        }
+
+        // watch user's input: When input changed, should query from SearchSuggestionsProvider
+        binding.searchInput.doAfterTextChanged {editable ->
+            userInput = editable.toString()
         }
 
 
+        // when user click search button on soft keyboard, call viewModel function to fetch data
+        binding.searchInput.setOnEditorActionListener { textView, actionId, keyEvent ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                viewModel.getImagesFromPixabayAPI(userInput)
+                true
+            }else{
+                false
+            }
+        }
     }
 }
 
