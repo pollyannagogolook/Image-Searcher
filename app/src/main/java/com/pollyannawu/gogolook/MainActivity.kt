551c95fa91463f375f47de85077ddbc0fec1e17e
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-25 22:43:20 +0800

    create hideKeyboard function to hide soft keyboard when click search btn on keyboard

--- app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
+++ app/src/main/java/com/pollyannawu/gogolook/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.pollyannawu.gogolook
 
+import android.content.Context
 import android.graphics.ColorSpace.Adaptation
 import android.graphics.drawable.GradientDrawable.Orientation
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
 import androidx.core.widget.doAfterTextChanged
@@ -14,6 +17,8 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.pollyannawu.gogolook.data.dataclass.Result
+import com.pollyannawu.gogolook.data.dataclass.succeeded
 import com.pollyannawu.gogolook.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
@@ -47,9 +52,23 @@ class MainActivity : ComponentActivity() {
                     binding.imageRecyclerview.layoutManager = GridLayoutManager(this@MainActivity, GRID_COUNT_SPAN)
                 }
                 binding.imageRecyclerview.adapter = imageAdapter
+
+
+                viewModel.resultList.collect{result ->
+                    if (result is Result.Success){
+                    imageAdapter.submitList(result.data)
+                        Log.i(TAG, "result: ${result.succeeded}")
+                    }else{
+                        Log.i(TAG, "result: ${result.succeeded}")
+                    }
+                }
             }
 
+
         }
+
+
+
 
         // watch user's input: When input changed, should query from SearchSuggestionsProvider
         binding.searchInput.doAfterTextChanged {editable ->
@@ -57,14 +76,26 @@ class MainActivity : ComponentActivity() {
         }
 
 
+
+
         // when user click search button on soft keyboard, call viewModel function to fetch data
         binding.searchInput.setOnEditorActionListener { textView, actionId, keyEvent ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH){
+                hideKeyboard()
                 viewModel.getImagesFromPixabayAPI(userInput)
                 true
             }else{
                 false
             }
+        }
+    }
+
+
+    // when user click search, should hide the soft keyboard
+    private fun hideKeyboard(){
+        this.currentFocus.let { view ->
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            imm?.hideSoftInputFromWindow(view?.windowToken, 0)
         }
     }
 }
