b44b98a650333a73718372e0f954d59bcfd0c522
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-27 21:59:00 +0800

    register suggestionProvider in manifest

--- app/src/main/java/com/pollyannawu/gogolook/SuggestionProvider.kt
+++ app/src/main/java/com/pollyannawu/gogolook/SuggestionProvider.kt
@@ -1,4 +1,14 @@
 package com.pollyannawu.gogolook
 
-class SuggestionProvider {
+import android.content.SearchRecentSuggestionsProvider
+
+class SuggestionProvider: SearchRecentSuggestionsProvider() {
+    init {
+        setupSuggestions(AUTHORITY, MODE)
+    }
+
+    companion object{
+        const val AUTHORITY = "com.pollyannawu.gogolook.SuggestionProvider"
+        const val MODE: Int = DATABASE_MODE_QUERIES
+    }
 }
