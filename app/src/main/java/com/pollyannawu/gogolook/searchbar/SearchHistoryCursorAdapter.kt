127675bf99862af4a27cae5dc613eb6b42be3f16
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-28 12:13:26 +0800

    create a cursorAdapter to bind history data and history viewholder

---
+++ app/src/main/java/com/pollyannawu/gogolook/searchbar/SearchHistoryCursorAdapter.kt
@@ -0,0 +1,18 @@
+package com.pollyannawu.gogolook.searchbar
+
+import android.content.Context
+import android.database.Cursor
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CursorAdapter
+
+class SearchHistoryCursorAdapter(context: Context, cursor: Cursor): CursorAdapter(context, cursor, 0) {
+    override fun newView(p0: Context?, p1: Cursor?, p2: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun bindView(p0: View?, p1: Context?, p2: Cursor?) {
+        TODO("Not yet implemented")
+    }
+
+}
