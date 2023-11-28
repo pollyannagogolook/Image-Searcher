cc8ca816af493df8d6edea681b2c1875ec7617bd
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-28 13:46:14 +0800

    bind search data and viewholder layout with data binding in SearchHistoryCursorAdapter

--- app/src/main/java/com/pollyannawu/gogolook/searchbar/SearchHistoryCursorAdapter.kt
+++ app/src/main/java/com/pollyannawu/gogolook/searchbar/SearchHistoryCursorAdapter.kt
@@ -1,18 +1,27 @@
 package com.pollyannawu.gogolook.searchbar
 
+import android.app.SearchManager
 import android.content.Context
 import android.database.Cursor
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CursorAdapter
+import androidx.databinding.DataBindingUtil
+import com.pollyannawu.gogolook.databinding.SearchHistoryViewholderBinding
 
 class SearchHistoryCursorAdapter(context: Context, cursor: Cursor): CursorAdapter(context, cursor, 0) {
-    override fun newView(p0: Context?, p1: Cursor?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+
+    override fun newView(context: Context, cursor: Cursor, parent: ViewGroup): View {
+        val binding = SearchHistoryViewholderBinding.inflate(LayoutInflater.from(context), parent, false)
+        return binding.root
     }
 
-    override fun bindView(p0: View?, p1: Context?, p2: Cursor?) {
-        TODO("Not yet implemented")
+    override fun bindView(view: View, context: Context, cursor: Cursor?) {
+        val binding = DataBindingUtil.getBinding<SearchHistoryViewholderBinding>(view)
+        val searchItem = cursor?.getString(cursor.getColumnIndexOrThrow(SearchManager.SUGGEST_COLUMN_TEXT_1))
+
+        binding?.let { binding.searchItem = searchItem }
     }
 
 }
