b31d8409adb2839b9320606a58c7cdab6ec52686
PY Wuu <114213570+pollyannaanalytics@users.noreply.github.com>
2023-11-29 18:45:01 +0800

    use hilt to produce ApiService singleton object in AppModuleClass

--- app/src/main/java/com/pollyannawu/gogolook/data/di/AppModuleClass.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/di/AppModuleClass.kt
@@ -4,18 +4,72 @@ import com.pollyannawu.gogolook.data.model.RemoteDataSource
 import com.pollyannawu.gogolook.data.model.RemoteDataSourceImpl
 import com.pollyannawu.gogolook.data.model.Repository
 import com.pollyannawu.gogolook.data.model.RepositoryImpl
+import com.pollyannawu.gogolook.network.ApiService
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class AppModuleClass {
+
+
 
     @Binds
     abstract fun provideRemoteDataSource(remoteDataSourceImpl: RemoteDataSourceImpl): RemoteDataSource
 
     @Binds
     abstract fun provideRepository(repositoryImpl: RepositoryImpl): Repository
+
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val url = original.url.newBuilder().build()
+                val request = original.newBuilder().url(url).build()
+                chain.proceed(request)
+            }
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMoshi(): Moshi {
+        return Moshi.Builder()
+            .addLast(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .baseUrl(BASE_URL)
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
+    companion object {
+        private const val HOST_NAME = "pixabay.com"
+        private const val BASE_URL = "https://${HOST_NAME}/api/"
+    }
 }
