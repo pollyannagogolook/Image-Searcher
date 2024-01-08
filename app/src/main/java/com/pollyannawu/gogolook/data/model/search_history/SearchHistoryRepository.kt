6002179b0ce3626f3b19535b33ea69e591b443a4
Pollyanna Wu <pollyanna.wu@gogolook.com>
2024-01-08 18:51:09 +0800

    create SearchHistoryRepository in search_history folder

---
+++ app/src/main/java/com/pollyannawu/gogolook/data/model/search_history/SearchHistoryRepository.kt
@@ -0,0 +1,6 @@
+package com.pollyannawu.gogolook.data.model.search_history
+
+class SearchHistoryRepository {
+    fun saveSearchQuery(query: String){}
+    fun updateSearchHistorySuggestion(query: String){}
+}
