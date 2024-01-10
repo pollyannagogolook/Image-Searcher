1ae10a3deed21dc4a3a7477e9b0d9c4cf4a0bc5b
Pollyanna Wu <pollyanna.wu@gogolook.com>
2024-01-10 14:29:57 +0800

    Feat: Create RemoteConfigRepository class
    - Create a RemoteConfigRepository class to control remoteConfigManager by repository pattern
    - RemoteConfigRepository is also a singleton object, inject remoteConfigManager by Hilt

--- app/src/main/java/com/pollyannawu/gogolook/data/model/remote_config/FirebaseRemoteConfigManagerExt.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/model/remote_config/FirebaseRemoteConfigManagerExt.kt
@@ -5,34 +5,49 @@ import com.google.firebase.remoteconfig.BuildConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
 import com.google.firebase.remoteconfig.get
+import com.pollyannawu.gogolook.R
 import com.pollyannawu.gogolook.data.model.RemoteDataSourceImpl
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class FirebaseRemoteConfigManagerExt @Inject constructor(): RemoteConfig {
+    companion object {
+        const val TAG = "FirebaseRemoteConfigManagerExt"
+    }
 
+    private var firebaseRemoteConfig: FirebaseRemoteConfig? = null
     init {
         initiateRemoteConfig()
     }
 
     private fun initiateRemoteConfig(){
-       val builder = FirebaseRemoteConfigSettings.Builder()
-
-        if(BuildConfig.DEBUG){
-            builder.minimumFetchIntervalInSeconds = 0L
+        if (firebaseRemoteConfig !== null) {
+            return
         }
-        val configSettings = builder.build()
-        FirebaseRemoteConfig.getInstance().setConfigSettingsAsync(configSettings)
+
+       firebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
+
+        // set default value
+        firebaseRemoteConfig?.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        // fetch remote config
+        firebaseRemoteConfig?.fetchAndActivate()?.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d(TAG, "FirebaseRemoteConfigManagerExt fetchAndActivate success")
+            } else {
+                Log.d(TAG, "FirebaseRemoteConfigManagerExt fetchAndActivate failed")
+            }
+        }
     }
 
     override fun getString(key: String, fallback: String): String {
-        return FirebaseRemoteConfig.getInstance()[key].run{
-            try {
-                asString()
-            } catch (e: IllegalArgumentException) {
-                fallback
-            }
+        val config = firebaseRemoteConfig?.get(key)?: FirebaseRemoteConfig.getInstance().get(key)
+
+        return try {
+            config.asString()
+        }catch (e: IllegalArgumentException){
+            fallback
         }
     }
 }
--- app/src/main/java/com/pollyannawu/gogolook/data/model/remote_config/RemoteConfigRepository.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/model/remote_config/RemoteConfigRepository.kt
@@ -4,6 +4,8 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class RemoteConfigRepository @Inject constructor(remoteConfigManagerExt: FirebaseRemoteConfigManagerExt) {
-    fun getDefaultLayout(defaultLayoutCallback: (String) -> Unit){}
+class RemoteConfigRepository @Inject constructor(private val remoteConfigManagerExt: RemoteConfig) {
+    fun getDefaultLayout(key: String, fallback: String): String{
+        return remoteConfigManagerExt.getString(key, fallback)
+    }
 }
