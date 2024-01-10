d7020e4f995cd053ab0752457e394602df4bc387
Pollyanna Wu <pollyanna.wu@gogolook.com>
2024-01-10 14:52:04 +0800

    Feat: Create SearchHistoryDataSource class - Create SearchHistoryDataSource class as a singleton object to focus ondeal with  searching history. - In the class, there are two methods: updateSearchHsitorySuggestion and saveSearchQuery. - the class use content provider to get local data, will return SQL table data with a cursor.

--- app/src/main/java/com/pollyannawu/gogolook/data/model/search_history/SearchHistoryRepository.kt
+++ app/src/main/java/com/pollyannawu/gogolook/data/model/search_history/SearchHistoryRepository.kt
@@ -1,9 +1,16 @@
 package com.pollyannawu.gogolook.data.model.search_history
 
 import com.google.firestore.v1.Cursor
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class SearchHistoryRepository {
+@Singleton
+class SearchHistoryRepository @Inject constructor(private val searchHistoryDataSource: SearchHistoryDataSource){
     fun saveSearchQuery(query: String){
+        searchHistoryDataSource.saveSearchQuery(query)
+
     }
-    fun updateSearchHistorySuggestion(query: String){}
+    fun updateSearchHistorySuggestion(query: String){
+        searchHistoryDataSource.updateSearchHistorySuggestion(query)
+    }
 }
